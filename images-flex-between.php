708d4c59f8fa51b695f8172f443131151b505bdd
Veronika Hanzlíková <hanzlikova@shoptet.cz>
2018-07-22 20:54:03 +0200

    Cleanup after refactoring

--- images-flex-between.php
+++ images-flex-between.php
@@ -1,4 +1,24 @@
-<?php /* Template Name: Images Flex Justify Between template */ ?>
+<?php /* Template Name: Flex - Justify Between */ ?>
+
+<?php if( !isset($singleView) || $singleView ) { get_header();  ?>
+
+<section class="section">
+    <div class="section-inner container">
+        <?php get_template_part( 'template-parts/utils/content', 'breadcrumb' ); ?>
+        <h1><?php the_title(); ?></h1>
+    </div>
+</section>
+
+<section class="section section-tertiary offset-images">
+    <div class="section-inner container offset-images-content">
+        <div class="d-flex flex-row justify-content-between">
+            <?php the_content(); ?>
+        </div>
+        <?php get_template_part( 'template-parts/page/content', 'widget' ); ?>
+    </div>
+</section>
+
+<?php get_footer(); } else { ?>
 
 <section class="section section-tertiary offset-images">
     <div class="section-inner container offset-images-content">
@@ -7,3 +27,5 @@
         </div>
     </div>
 </section>
+
+<?php } ?>
--- images-flex-centered.php
+++ images-flex-centered.php
@@ -1,4 +1,19 @@
-<?php /* Template Name: Images Flex Justify Centered template */ ?>
+<?php /* Template Name: Flex - Justify Centered */ ?>
+
+<?php if( !isset($singleView) || $singleView ) { get_header();  ?>
+
+<section class="section">
+    <div class="section-inner container">
+        <?php get_template_part( 'template-parts/utils/content', 'breadcrumb' ); ?>
+        <h1><?php the_title(); ?></h1>
+        <div class="d-flex flex-row justify-content-center children-mx-20">
+            <?php the_content(); ?>
+        </div>
+        <?php get_template_part( 'template-parts/page/content', 'widget' ); ?>
+    </div>
+</section>
+
+<?php get_footer(); } else { ?>
 
 <section class="section">
     <div class="section-inner container">
@@ -7,3 +22,7 @@
         </div>
     </div>
 </section>
+
+<?php } ?>
+
+
